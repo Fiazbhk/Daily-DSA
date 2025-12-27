f963b7fa0e98e85cfe67d1e5d8f517f994aca7dc
Muhammad Fiaz <110216181+Fiazbhk@users.noreply.github.com>
2025-12-27 19:19:43 +0500

    Time: 3 ms (31.89%) | Memory: 18.3 MB (81.82%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        
+        s = set(nums)
+
+        if len(s) < 3:
+            return max(s)
+        elif len(s) == 3:
+            return min(s)
+        else:
+            s.remove(max(s))
+            s.remove(max(s))
+            return max(s)
+
+        # Time Complexity = O(n)
+        # Space Complexity = O(n)
